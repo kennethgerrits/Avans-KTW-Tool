9c7930724a6d13c25677cd18e7f47001c21cd897
Kenneth Gerrits <kennethgerrits@gmail.com>
2020-01-19 17:34:15 +0100

    style fix + cleanup

--- AvansKTWTool/AvansKTWTool/ViewModel/CalculateNotRegistered.cs
+++ AvansKTWTool/AvansKTWTool/ViewModel/CalculateNotRegistered.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
-using Microsoft.Win32;
 
 namespace AvansKTWTool.ViewModel
 {
@@ -22,61 +19,54 @@ namespace AvansKTWTool.ViewModel
         public ICommand CalculateNotRegisteredCommand { get; set; }
 
         //private fields
-        private List<string> _allKanidatesList;
-        private List<string> _chosenKanidatesList;
         private List<string> _leftoverKanidatesList;
-        
+
         //constructor
         public CalculateNotRegistered()
         {
             //Init
-            _allKanidatesList = new List<string>();
-            _chosenKanidatesList = new List<string>();
             _leftoverKanidatesList = new List<string>();
 
             //RelayCommands
             CalculateNotRegisteredCommand = new RelayCommand(CalculateNotRegisteredKanidates);
-
         }
 
         private void CalculateNotRegisteredKanidates()
         {
             if (AllKanidatesInformation.Equals("") || ChosenKanidatesInformation.Equals("")) return;
 
-            List<string> _allKanidatesList = AllKanidatesInformation.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
-                .ToList();   
-            
-            List<string> _chosenKanidatesList = ChosenKanidatesInformation.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+            List<string> allKanidatesList = AllKanidatesInformation.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
                 .ToList();
 
-            _leftoverKanidatesList = _allKanidatesList.Except(_chosenKanidatesList).ToList();
+            List<string> chosenKanidatesList = ChosenKanidatesInformation.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
 
-            string fileName = @"D:\Kanidates.txt"; 
+            _leftoverKanidatesList = allKanidatesList.Except(chosenKanidatesList).ToList();
+
+            string filePath = @"D:\Kanidates.txt";
 
             try
             {
-                if (File.Exists(fileName))
-                    File.Delete(fileName);
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
 
-                using (StreamWriter sw = File.CreateText(fileName))
+                using (StreamWriter sw = File.CreateText(filePath))
                 {
-                    sw.WriteLine("The following kanidates are not registered:");
+                    sw.WriteLine("The following kanidates are not scheduled:");
                     sw.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
                     foreach (var name in _leftoverKanidatesList)
-                    {
                         sw.WriteLine(name);
-                    }
-
+                    
                     sw.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
                     sw.WriteLine("Export date: {0}", DateTime.Now.ToString(CultureInfo.CurrentCulture));
                     sw.WriteLine("Application made by {0}", "Kenneth Gerrits: github.com/kennethgerrits");
                 }
-                MessageBox.Show($"Exported file succesfully to: {fileName} ", "Here you go!", MessageBoxButton.OK);
+                MessageBox.Show($"Exported file succesfully to: {filePath} ", "Here you go!", MessageBoxButton.OK);
             }
-            catch (Exception Ex)
+            catch (Exception ex)
             {
-                Console.WriteLine(Ex.ToString());
+                Console.WriteLine(ex.ToString());
             }
 
         }
